c17821da11a9cf0ce1b0e6daa50f80bda34afe59
jerannn24 <junioroppo64@gmail.com>
2025-08-07 16:16:51 +0700

    Add : HandleAction

--- app/page.js
+++ app/page.js
@@ -12,19 +12,36 @@ const App = () => {
   };
 
   const handleAction = () => {
+    if (userinput.trim() === '') return;
 
+    if (editIndex !== null) {
+      const updateList = list.map((item, index) =>
+        index === editIndex ? { ...item, value: userinput } : item
+      );
+      setList(updateList);
+      setEditIndex(null);
+    } else {
+      const newItem = {
+        id: Math.random(),
+        value: userinput,
+      };
+      setList([...list, newItem]);
+    }
+    setUserInput('');
   }
   return (
     <div id='divBody' >
       <div id='title'>ToDo List</div>
       <div id='divTodo'>
+
         <input
           id="textInput"
           placeholder={editIndex !== null ? "Edit Item..." : "Add Item"}
           value={userinput}
           onChange={(e) => updateInput(e.target.value)}>
         </input>
-        <button id="button" >{editIndex !== null ? "Update" : "Add"}</button>
+
+        <button id="button" onClick={handleAction}>{editIndex !== null ? "Update" : "Add"}</button>
       </div>
     </div >
   )
